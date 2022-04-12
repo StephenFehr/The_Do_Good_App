818d61da23f42a9ae64c8f5e69b957e6e548de96
StephenFehr <fehrstephend@gmail.com>
2022-04-11 22:02:27 -0700

    Add files via upload

---
+++ src/components/GetMonthDate.jsx
@@ -0,0 +1,20 @@
+export function FirstofMonth(num){
+    //Function which returns the first of the previous month
+    //Includes a parameter to decrement the number of months in the past
+    //when num is 0 it is the current month when num is 1 it is last month
+
+    var date = new Date();
+    var firstDay = new Date(date.getFullYear(), date.getMonth()-num, 1);
+    
+    return firstDay.toISOString();
+}
+
+export function LastOfMonth(num){
+    //Function which returns a date for the last day of the previous month
+    //Includes a parameter to decrement the number of months in the past
+    //when num is -1 it is the current month when num is 0 it is last month
+    var date = new Date();
+    var lastDay = new Date(date.getFullYear(), date.getMonth()-num, 0);
+
+    return lastDay.toISOString();
+}
